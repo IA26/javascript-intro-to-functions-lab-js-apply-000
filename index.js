910783c56b0a8898f32a593510ff876947e9d2cd
Isaac Avilez <isaac1180@aol.com>
2019-01-02 03:22:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function sayHiToGrandma(string) {
   if (string == string.toLowerCase()) {
     return "I can't hear you!"
   }
-  if (string == string.toUpperCase()) {
+  else if (string == string.toUpperCase()) {
     return "YES INDEED!"
   }
-  if (string = "I love u, Grandma") {
+  else if (string = "I love u, Grandma") {
     return "I love you, too"
   }
   
